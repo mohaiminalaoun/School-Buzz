8107e993fb380fdd12ac3b419e32bfc9e077cfb0
Mohaimin Al Aoun <mohaimin.al.aoun@vanderbilt.edu>
2017-06-21 23:25:05 -0400

    Can cancel sign in process

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.google.firebase.udacity.friendlychat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -150,36 +151,7 @@ public class MainActivity extends AppCompatActivity {
                 mMessageEditText.setText("");
             }
         });
-        mChildEventListener = new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                //snapshot is current snapshot, then we seseriaze the snapshot to FM
-                FriendlyMessage friendlyMessage = dataSnapshot.getValue(FriendlyMessage.class);
-                mMessageAdapter.add(friendlyMessage);
-            }
 
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        };
-
-        mMessagesDatabaseReference.addChildEventListener(mChildEventListener);
 
         // create auth listener // type new authstatelistener and wait for autocomplete :p
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
@@ -188,7 +160,7 @@ public class MainActivity extends AppCompatActivity {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if(user!=null){
                     // is user is signed in
-                    Toast.makeText(MainActivity.this, "You're logged in!", Toast.LENGTH_SHORT).show();
+                    onSignedInInitialize(user.getDisplayName());
                 }else{
                     // if user is not signed in
                     startActivityForResult(
@@ -205,6 +177,76 @@ public class MainActivity extends AppCompatActivity {
         };
     }
 
+    // method that comes back from the sign in activity of Firebase
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode==RC_SIGN_IN){
+            if(resultCode==RESULT_OK){
+                Toast.makeText(MainActivity.this, "Signed in!", Toast.LENGTH_SHORT).show();
+            }else if (resultCode==RESULT_CANCELED){
+                Toast.makeText(MainActivity.this, "Cancelled", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void onSignedInInitialize(String userName) {
+        mUsername = userName;
+        // can read messages when you're signed in only
+        attachDatabaseReadListener();
+
+    }
+
+    // can't see messages when logged out
+    private void onSignedOutCleanup(){
+        mUsername = ANONYMOUS;
+        mMessageAdapter.clear();
+        detachDatabaseReadLisener();
+    }
+
+    // remove the listener for the messages
+    private void detachDatabaseReadLisener() {
+        if(mChildEventListener!=null) {
+            mMessagesDatabaseReference.removeEventListener(mChildEventListener);
+            mChildEventListener=null;
+        }
+    }
+
+    private void attachDatabaseReadListener(){
+        if(mChildEventListener==null) {
+            mChildEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    //snapshot is current snapshot, then we seseriaze the snapshot to FM
+                    FriendlyMessage friendlyMessage = dataSnapshot.getValue(FriendlyMessage.class);
+                    mMessageAdapter.add(friendlyMessage);
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            };
+
+            mMessagesDatabaseReference.addChildEventListener(mChildEventListener);
+        }
+    }
+
     //attach listener in onresume
     @Override
     protected void onResume(){
@@ -216,7 +258,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause(){
         super.onPause();
-        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+       if(mAuthStateListener!=null) mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        detachDatabaseReadLisener();
+        mMessageAdapter.clear();
     }
 
     @Override
