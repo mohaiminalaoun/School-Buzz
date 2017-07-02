39f4e161963b505564bd8ea7c4d6335c4bb0dc37
Mohaimin Al Aoun <mohaimin.al.aoun@vanderbilt.edu>
2017-07-02 17:22:15 -0400

    Sliding tab bar is working

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -2,6 +2,7 @@ package com.google.firebase.udacity.friendlychat;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -27,6 +28,15 @@ public class MainActivity extends AppCompatActivity
 
         // Set the adapter onto the view pager
         viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+
+        // Connect the tab layout with the view pager. This will
+        //   1. Update the tab layout when the view pager is swiped
+        //   2. Update the view pager when a tab is selected
+        //   3. Set the tab layout's tab names with the view pager's adapter's titles
+        //      by calling onPageTitle()
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     @Override
--- app/src/main/java/com/google/firebase/udacity/friendlychat/SimpleFragmentPagerAdapter.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/SimpleFragmentPagerAdapter.java
@@ -3,6 +3,7 @@ package com.google.firebase.udacity.friendlychat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
 
 /**
  * Created by Mohaimin on 7/2/2017.
@@ -10,11 +11,16 @@ import android.support.v4.app.FragmentPagerAdapter;
 class SimpleFragmentPagerAdapter  extends FragmentPagerAdapter {
 
     SimpleFragmentPagerAdapter(FragmentManager fm) {
+
         super(fm);
+        Log.e("SFPA: ","ctor called");
     }
+
+    private String tabTitles[] = new String[] { "Status", "Chats", "Profile" };
 
     @Override
     public Fragment getItem(int position) {
+        Log.e("SFPA: ","Get Item called");
         if (position == 0) {
             return new StatusFragment();
         } else if (position == 1){
@@ -26,6 +32,14 @@ class SimpleFragmentPagerAdapter  extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
+        Log.e("SFPA: ","Get count");
         return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        Log.e("SFPA: ", "getPageTitle called");
+        return tabTitles[position];
     }
 }
