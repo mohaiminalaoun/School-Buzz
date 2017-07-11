abf7b9c0804b52ab9958929aaa4442685f2e14da
Mohaimin Al Aoun <mohaimin.al.aoun@vanderbilt.edu>
2017-07-10 21:40:39 -0400

    set up testing dependencies for instrumentation testing'

---
+++ app/src/test/java/com/google/firebase/udacity/friendlychat/ChatActivityTest.java
@@ -0,0 +1,16 @@
+package com.google.firebase.udacity.friendlychat;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Mohaimin on 7/10/2017.
+ */
+public class ChatActivityTest {
+    @Test
+    public void getImageUri() throws Exception {
+        //TODO: write a test for this using instrumentation
+    }
+
+}
